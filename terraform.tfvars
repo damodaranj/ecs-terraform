4bcfffbc0437fab609de81facf6c2d482b8513a6
damodaranj <40349259+damodaranj@users.noreply.github.com>
2023-08-10 15:56:59 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+aws_region        = "us-east-1"
+aws_access_key    = "AKIAWFMZTA4FFDJAQV74"
+aws_secret_key    = "IJBYE+Tu59UTWTChcUQ5M4HuR0hBb8RUW4ZPvLLV"
+
+# these are zones and subnets examples
+availability_zones = ["us-east-1a", "us-east-1b"]
+public_subnets     = ["10.10.100.0/24", "10.10.101.0/24"]
+private_subnets    = ["10.10.0.0/24", "10.10.1.0/24"]
+
+# these are used for tags
+app_name        = "test-app"
+app_environment = "development"
---
+++ variable.tf
@@ -0,0 +1,47 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_cloudwatch_retention_in_days" {
+  type        = number
+  description = "AWS CloudWatch Logs Retention in Days"
+  default     = 1
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "Application Environment"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}
